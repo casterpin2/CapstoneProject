cd672936c0b580666bbc021bd58c2eeb19c67e6e
datduc23456 <datduc234562gmail.com>
2018-06-12 21:54:15 +0700

    no message

---
+++ Capstone Project/Project Deliverables/Draft/Back-End Api/3sProjectFinal/src/java/com/service/ProductService.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.service;
+
+import com.entites.ProductAddEntites;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author AHBP
+ */
+public interface ProductService {
+     public List<ProductAddEntites> getProductForAdd(String query) throws SQLException;
+}
---
+++ Capstone Project/Project Deliverables/Draft/Back-End Api/3sProjectFinal/src/java/com/service/ProductServiceImpl.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.service;
+
+import com.dao.ProductDao;
+import com.entites.ProductAddEntites;
+import java.sql.SQLException;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author AHBP
+ */
+@Service("productService")
+public class ProductServiceImpl implements ProductService{
+    @Autowired
+    ProductDao dao;
+
+    @Override
+    public List<ProductAddEntites> getProductForAdd(String query) throws SQLException {
+        return dao.getProductForAdd(query);
+    }
+}
