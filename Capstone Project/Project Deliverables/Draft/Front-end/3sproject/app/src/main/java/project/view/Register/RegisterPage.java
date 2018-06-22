fe6b610d326db0d78d7a2269d52f945fbea0334a
Thang Nguyen <sexinmyeye@gmail.com>
2018-06-21 20:50:38 -0700

    giao diện màn Đăng kí

--- Capstone Project/Project Deliverables/Draft/Front-end/3sproject/app/src/main/java/project/view/Register/RegisterPage.java
+++ Capstone Project/Project Deliverables/Draft/Front-end/3sproject/app/src/main/java/project/view/Register/RegisterPage.java
@@ -7,11 +7,18 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import project.view.Login.LoginPage;
 import project.view.MainActivity;
 import project.view.R;
 
@@ -19,6 +26,12 @@ public class RegisterPage extends AppCompatActivity {
 
     private Context context;
     private View view;
+    private ScrollView scroll;
+    private TextView usernameValue, passwordValue, rePasswordValue, emailValue, phoneValue, toRulePage, toLoginPage;
+    private TextView usernameErrorMessage, passwordErrorMessage, confirmPasswordErrorMessage, emailErrorMessage, phoneErrorMessage;
+    private CheckBox acceptRuleCheckBox;
+    private Button registerBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +40,46 @@ public class RegisterPage extends AppCompatActivity {
 
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.register_page_actionbar_custom);
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable((getColor(R.color.colorApplication))));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        scroll = (ScrollView) findViewById(R.id.scrollView);
+        usernameValue = (TextView) findViewById(R.id.passwordValue);
+        passwordValue = (TextView) findViewById(R.id.usernameValue);
+        rePasswordValue = (TextView) findViewById(R.id.rePasswordValue);
+        emailValue = (TextView) findViewById(R.id.emailValue);
+        phoneValue = (TextView) findViewById(R.id.phoneValue);
+        toRulePage = (TextView) findViewById(R.id.toRulePage);
+        toLoginPage = (TextView) findViewById(R.id.toLoginPage);
+        usernameErrorMessage = (TextView) findViewById(R.id.usernameErrorMessage);
+        passwordErrorMessage = (TextView) findViewById(R.id.passwordErrorMessage);
+        acceptRuleCheckBox = (CheckBox) findViewById(R.id.checkBox);
+        registerBtn = (Button) findViewById(R.id.registerBtn);
+
+
+        scroll.setVerticalScrollBarEnabled(false);
+        scroll.setHorizontalScrollBarEnabled(false);
+
+        String username = (String) usernameValue.getText().toString();
+        final String password = (String) passwordValue.getText().toString();
+        final String confirmPassword = (String) rePasswordValue.getText().toString();
+        String email = (String) emailValue.getText().toString();
+        String phone = (String) phoneValue.getText().toString();
+        boolean isAcceptRule = (Boolean) acceptRuleCheckBox.isChecked();
+
+
+        toLoginPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toLoginPage = new Intent(RegisterPage.this, LoginPage.class);
+                startActivity(toLoginPage);
+            }
+        });
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 
