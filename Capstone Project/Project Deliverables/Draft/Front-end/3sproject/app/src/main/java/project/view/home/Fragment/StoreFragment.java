b7623c542d642b92235c07823c1f39a7877b6b2e
Tung <tungvs96@gmail.com>
2018-07-12 01:01:47 -0700

    ghép màn home

--- Capstone Project/Project Deliverables/Draft/Front-end/3sproject/app/src/main/java/project/view/home/Fragment/StoreFragment.java
+++ Capstone Project/Project Deliverables/Draft/Front-end/3sproject/app/src/main/java/project/view/home/Fragment/StoreFragment.java
@@ -1,20 +1,33 @@
 package project.view.home.Fragment;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import project.view.ProductInStore.ProductInStoreDisplayPage;
 import project.view.R;
+import project.view.StoreInformation.StoreInformation;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class StoreFragment extends Fragment {
-
+    private RelativeLayout notHaveStoreLayout, haveStoreLayout, productLayout, orderLayout, storeImgLayout;
+    private TextView storeName, numberOfOrder, numberOfProduct, ownerName, address, registerDate, phoneText;
+    private ImageView storeImg, btnEdit;
+    private int storeID = 1;
+    private StoreInformation storeInformation;
+    private View view;
 
     public StoreFragment() {
         // Required empty public constructor
@@ -25,7 +38,148 @@ public class StoreFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_store, container, false);
+        view = inflater.inflate(R.layout.fragment_store,container,false);
+        storeInformation = new StoreInformation();
+
+        notHaveStoreLayout = view.findViewById(R.id.notHaveStoreLayout);
+        haveStoreLayout = view.findViewById(R.id.haveStoreLayout);
+        productLayout = view.findViewById(R.id.productLayout);
+        orderLayout = view.findViewById(R.id.orderLayout);
+        storeImgLayout = view.findViewById(R.id.storeImgLayout);
+
+        storeName = view.findViewById(R.id.storeName);
+        numberOfOrder = view.findViewById(R.id.numberOfOrder);
+        numberOfProduct = view.findViewById(R.id.numberOfProduct);
+        ownerName = view.findViewById(R.id.ownerName);
+        address = view.findViewById(R.id.address);
+        registerDate = view.findViewById(R.id.registerDate);
+        phoneText = view.findViewById(R.id.phoneText);
+        storeImg = view.findViewById(R.id.storeImg);
+        btnEdit = view.findViewById(R.id.btnEdit);
+
+
+        // set layout in case of store null or not
+        // storeId is transported from login action
+        //storeID = getIntent().getIntExtra("storeID", -1);
+        setLayout(storeID,haveStoreLayout,notHaveStoreLayout);
+
+        productLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toStoreProduct = new Intent(getContext(), ProductInStoreDisplayPage.class);
+                toStoreProduct.putExtra("storeID", storeID);
+                startActivity(toStoreProduct);
+            }
+        });
+
+        orderLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        // click on to choose image from gallery
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        int storeID = getActivity().getIntent().getIntExtra("storeID", -1);
+        String nameStoree = storeName.getText().toString();
+        int numberOfOrderStoree = Integer.parseInt(numberOfOrder.getText().toString());
+        int numberOfProductStoree = Integer.parseInt(numberOfProduct.getText().toString());
+        String ownerNameStoree = ownerName.getText().toString();
+        String addressStoree = address.getText().toString();
+        String registerDateStoree = registerDate.getText().toString();
+        String phoneTextStoree = phoneText.getText().toString();
+
+        savingPreferences(storeID, nameStoree, numberOfOrderStoree, numberOfProductStoree, ownerNameStoree, addressStoree, registerDateStoree, phoneTextStoree);
+
+        return view;
+    }
+
+
+    @Override
+    public void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+        //gọi hàm đọc trạng thái ở đây
+        restoringPreferences();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        int storeID = getActivity().getIntent().getIntExtra("storeID", -1);
+        String nameStoree = storeName.getText().toString();
+        int numberOfOrderStoree = Integer.parseInt(numberOfOrder.getText().toString());
+        int numberOfProductStoree = Integer.parseInt(numberOfProduct.getText().toString());
+        String ownerNameStoree = ownerName.getText().toString();
+        String addressStoree = address.getText().toString();
+        String registerDateStoree = registerDate.getText().toString();
+        String phoneTextStoree = phoneText.getText().toString();
+
+        savingPreferences(storeID, nameStoree, numberOfOrderStoree, numberOfProductStoree, ownerNameStoree, addressStoree, registerDateStoree, phoneTextStoree);
+    }
+
+    public void setLayout(int storeID, RelativeLayout haveStore, RelativeLayout notHaveStore){
+        if (storeID == 0) {
+            haveStore.setVisibility(View.INVISIBLE);
+            notHaveStore.setVisibility(View.VISIBLE);
+        } else if(storeID > 0) {
+            haveStore.setVisibility(View.VISIBLE);
+            notHaveStore.setVisibility(View.INVISIBLE);
+        } else {
+
+        }
+    }
+
+    public void savingPreferences(int storeID, String name, int numberOfOrder, int numberOfProduct, String ownerName, String address, String registerDate, String phoneText){
+        //tạo đối tượng getSharedPreferences
+        SharedPreferences pre=this.getActivity().getSharedPreferences("storeInformation", Context.MODE_PRIVATE);
+        //tạo đối tượng Editor để lưu thay đổi
+        SharedPreferences.Editor editor=pre.edit();
+        //lưu vào editor
+        editor.putString("name", name);
+        editor.putInt("numberOfOrder", numberOfOrder);
+        editor.putInt("numberOfProduct", numberOfProduct);
+        editor.putString("ownerName", ownerName);
+        editor.putString("address", address);
+        editor.putString("registerDate", registerDate);
+        editor.putString("phoneText", phoneText);
+        editor.putInt("storeID", storeID);
+
+        //chấp nhận lưu xuống file
+        editor.commit();
+    }
+
+    public void restoringPreferences(){
+        SharedPreferences pre=this.getActivity().getSharedPreferences("storeInformation", Context.MODE_PRIVATE);
+        //lấy giá trị checked ra, nếu không thấy thì giá trị mặc định là false
+
+        //lấy user, pwd, nếu không thấy giá trị mặc định là rỗng
+        int storeID = pre.getInt("storeID", -1);
+        String nameStoree = pre.getString("name", "");
+        int numberOfOrderStoree = pre.getInt("numberOfOrder", 0);
+        int numberOfProductStoree = pre.getInt("numberOfProduct", 0);
+        String ownerNameStoree = pre.getString("ownerName", "");
+        String addressStoree = pre.getString("address", "");
+        String registerDateStoree = pre.getString("registerDate", "");
+        String phoneTextStoree = pre.getString("phoneText", "");
+
+
+        storeName.setText(nameStoree);
+        numberOfOrder.setText(String.valueOf(numberOfOrderStoree));
+        numberOfProduct.setText(String.valueOf(numberOfProductStoree));
+        ownerName.setText(ownerNameStoree);
+        address.setText(addressStoree);
+        registerDate.setText(registerDateStoree);
+        phoneText.setText(phoneTextStoree);
+
     }
 
 }
--- Capstone Project/Project Deliverables/Draft/Front-end/3sproject/app/src/main/java/project/view/home/Fragment/UserFragment.java
+++ Capstone Project/Project Deliverables/Draft/Front-end/3sproject/app/src/main/java/project/view/home/Fragment/UserFragment.java
@@ -1,19 +1,33 @@
 package project.view.home.Fragment;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import project.view.EditUserInformation.EditUserInformationPage;
 import project.view.R;
+import project.view.UserInformation.TweakUI;
+import project.view.UserInformation.UserInformationPage;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class UserFragment extends Fragment {
-
+    private TextView txtName, txtAddress, txtPhone, txtEmail, txtGender, txtDob, txtstoreName, txtOder;
+    ImageView btnEdit;
+    RelativeLayout storeLayout;
+    private View view;
+    private final int REQUEST_PROFILE_CODE = 123;
 
     public UserFragment() {
         // Required empty public constructor
@@ -24,7 +38,167 @@ public class UserFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_me, container, false);
+        view = inflater.inflate(R.layout.fragment_me,container,false);
+        TweakUI.makeTransparent(getActivity());
+        mapping();
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editProfile(v);
+            }
+        });
+
+        storeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        String name = txtName.getText().toString();
+        String address = txtAddress.getText().toString();
+        String phone = txtPhone.getText().toString();
+        String email = txtEmail.getText().toString();
+        String gender = txtGender.getText().toString();
+        String dob = txtDob.getText().toString();
+        int numberOfOrder = Integer.parseInt(txtOder.getText().toString());
+        String storeName = txtstoreName.getText().toString();
+
+        //gọi hàm lưu trạng thái ở đây
+        savingPreferences(name, address, phone, email, gender, dob, numberOfOrder, storeName);
+
+        return view;
+    }
+
+
+    @Override
+    public void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+
+        String name = txtName.getText().toString();
+        String address = txtAddress.getText().toString();
+        String phone = txtPhone.getText().toString();
+        String email = txtEmail.getText().toString();
+        String gender = txtGender.getText().toString();
+        String dob = txtDob.getText().toString();
+        int numberOfOrder = Integer.parseInt(txtOder.getText().toString());
+        String storeName = txtstoreName.getText().toString();
+
+        //gọi hàm lưu trạng thái ở đây
+        savingPreferences(name, address, phone, email, gender, dob, numberOfOrder, storeName);
+    }
+    @Override
+    public void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+        //gọi hàm đọc trạng thái ở đây
+        restoringPreferences();
+    }
+
+    public void editProfile(View view) {
+        Bundle extras = new Bundle();
+        extras.putString("name", txtName.getText().toString());
+        extras.putString("phone", txtPhone.getText().toString());
+        extras.putString("address", txtAddress.getText().toString());
+        extras.putString("email", txtEmail.getText().toString());
+        extras.putString("dob", txtDob.getText().toString());
+        extras.putString("gender", txtGender.getText().toString());
+
+        Intent toEditUserInformationPage = new Intent(getContext(), EditUserInformationPage.class);
+        toEditUserInformationPage.putExtras(extras);
+        startActivityForResult(toEditUserInformationPage, REQUEST_PROFILE_CODE);
+    }
+
+
+    private void mapping() {
+        btnEdit = view.findViewById(R.id.btnEdit);
+        txtName = view.findViewById(R.id.txt_name);
+        txtPhone = view.findViewById(R.id.txt_phone);
+        txtAddress = view.findViewById(R.id.txt_address);
+        txtEmail = view.findViewById(R.id.txt_email);
+        txtGender = view.findViewById(R.id.txt_gender);
+        txtDob = view.findViewById(R.id.txt_dob);
+        txtstoreName = view.findViewById(R.id.storeName);
+        storeLayout = view.findViewById(R.id.storeLayout);
+        txtOder = view.findViewById(R.id.txtOder);
+            }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_PROFILE_CODE && resultCode == 200) {
+            Bundle extras = data.getExtras();
+
+            String name = extras.getString("name");
+            String address = extras.getString("address");
+            String phone = extras.getString("phone");
+            String email = extras.getString("email");
+            String gender = extras.getString("gender");
+            String dob = extras.getString("dob");
+            int numberOfOrder = Integer.parseInt(txtOder.getText().toString());
+            String storeName = txtstoreName.getText().toString();
+
+
+            txtName.setText(name);
+            txtAddress.setText(address);
+            txtPhone.setText(phone);
+            txtEmail.setText(email);
+            txtDob.setText(dob);
+            txtGender.setText(gender);
+
+            savingPreferences(name, address, phone, email, gender, dob, numberOfOrder, storeName);
+        }
+        else if (requestCode == REQUEST_PROFILE_CODE && resultCode == 201) {
+            Toast.makeText(getContext(), "Thông tin không thay đổi", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void savingPreferences(String name, String address, String phone, String email, String gender, String dob, int numberOfOrder, String storeName){
+        //tạo đối tượng getSharedPreferences
+        SharedPreferences pre = this.getActivity().getSharedPreferences("main", Context.MODE_PRIVATE);
+        //tạo đối tượng Editor để lưu thay đổi
+        SharedPreferences.Editor editor=pre.edit();
+        //lưu vào editor
+        editor.putString("name", name);
+        editor.putString("address", address);
+        editor.putString("phone", phone);
+        editor.putString("email", email);
+        editor.putString("gender", gender);
+        editor.putString("dob", dob);
+        editor.putInt("numberOfOrder", numberOfOrder);
+        editor.putString("storeName",storeName );
+
+
+        //chấp nhận lưu xuống file
+        editor.commit();
+    }
+
+    public void restoringPreferences(){
+        SharedPreferences pre = this.getActivity().getSharedPreferences("main", Context.MODE_PRIVATE);
+        //lấy giá trị checked ra, nếu không thấy thì giá trị mặc định là false
+
+        //lấy user, pwd, nếu không thấy giá trị mặc định là rỗng
+        String name = pre.getString("name", "");
+        String address = pre.getString("address", "");
+        String phone = pre.getString("phone", "");
+        String email = pre.getString("email", "");
+        String gender = pre.getString("gender", "");
+        String dob = pre.getString("dob", "");
+
+//        Toast.makeText(this, "name" + name, Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, "address" + address, Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, "phone" + phone, Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, "gender" + gender, Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, "dob" + dob, Toast.LENGTH_SHORT).show();
+
+        txtName.setText(name);
+        txtAddress.setText(address);
+        txtPhone.setText(phone);
+        txtEmail.setText(email);
+        txtGender.setText(gender);
+        txtDob.setText(dob);
+
     }
 
 }
