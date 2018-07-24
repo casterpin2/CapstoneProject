92ce3a034771841e343f5f7a23215f752bcaad2f
datduc23456 <datduc234562gmail.com>
2018-07-24 18:56:21 +0700

    no message

--- Capstone Project/Project Deliverables/Draft/Front-end/3sproject/app/src/main/java/project/retrofit/APIService.java
+++ Capstone Project/Project Deliverables/Draft/Front-end/3sproject/app/src/main/java/project/retrofit/APIService.java
@@ -7,12 +7,15 @@ import project.googleMapAPI.GoogleMapJSON;
 import project.objects.User;
 import project.view.AddProductToStore.Item;
 import project.view.Brand.Brand;
+import project.view.Cart.Product;
 import project.view.Category.Category;
 import project.view.Login.Login;
 import project.view.ProductBrandDisplay.ProductBrand;
 import project.view.ProductInStore.ProductInStore;
+import project.view.Register.ResultRegister;
 import project.view.RegisterStore.Store;
 import project.view.SaleProduct.SaleProduct;
+import project.view.UserSearchProduct.NearByStore;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -66,8 +69,12 @@ public interface APIService {
                                        @Query("email") String email,@Query("phone") String phone,@Query("typeSearch") String typeSearch);
 
     @POST("registerUser")
-    @FormUrlEncoded
-    Call<Boolean> registerUserNew(@Field("jsonString") String jsonString);
+    Call<ResultRegister> registerUserNew(@Body User user);
+
+    //User search product
+    @GET("userSearchProduct")
+    Call<List<Product>> userSearchProduct(@Query("productName") String productName);
+
     @GET("productSales/top20")
     Call<List<SaleProduct>> getSaleProductTop20();
     @GET("productSales")
@@ -77,4 +84,7 @@ public interface APIService {
     @GET("json")
     Call<GoogleMapJSON> getLocation(@Query("latlng") String latlng,
                                     @Query("key") String key);
+
+    @GET("findStore")
+    Call<List<NearByStore>> nearByStore(@Query("productId") int productId,@Query("latitude") String latitude,@Query("longitude") String longitude);
 }
