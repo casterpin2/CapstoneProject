b1c72494d503756c5ae7d85b78d318d25f4a24b0
datduc23456 <datduc234562gmail.com>
2018-08-20 00:12:13 +0700

    no message

---
+++ Capstone Project/Project Deliverables/Draft/Front-end/3sproject/app/src/main/java/project/view/model/Feedback.java
@@ -0,0 +1,54 @@
+package project.view.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Feedback {
+    @SerializedName("user_id")
+    @Expose
+    private int user_id;
+    @SerializedName("store_id")
+    @Expose
+    private int store_id;
+    @SerializedName("content")
+    @Expose
+    private String content;
+    @SerializedName("isSatisfied")
+    @Expose
+    private int isSatisfied;
+
+    public Feedback() {
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getStore_id() {
+        return store_id;
+    }
+
+    public void setStore_id(int store_id) {
+        this.store_id = store_id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getIsSatisfied() {
+        return isSatisfied;
+    }
+
+    public void setIsSatisfied(int isSatisfied) {
+        this.isSatisfied = isSatisfied;
+    }
+}
