b3162f99e87abe4963f00e586880c916c88368da
datduc23456 <datduc234562gmail.com>
2018-08-20 23:13:24 +0700

    no message

--- Capstone Project/Project Deliverables/Draft/Front-end/3sproject/app/src/main/java/project/view/gui/FeedbackManagement.java
+++ Capstone Project/Project Deliverables/Draft/Front-end/3sproject/app/src/main/java/project/view/gui/FeedbackManagement.java
@@ -1,43 +1,169 @@
 package project.view.gui;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AbsListView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
+import project.retrofit.ApiUtils;
 import project.view.R;
 import project.view.adapter.FeedbackManagementAdapter;
-import project.view.model.Feedback;
+import project.view.model.Product;
+import project.view.model.UserFeedback;
 import project.view.util.CustomInterface;
+import retrofit2.Call;
+import retrofit2.Response;
 
 public class FeedbackManagement extends BasePage {
-
+    private List<UserFeedback> list;
+    private int storeId;
+    private FeedbackManagementAdapter feedbackManagementAdapter;
+    private ListView listView;
+    public View footerView;
+    public boolean isLoading;
+    private int page;
+    public Handler mHandle;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_listview);
-
+        list = new ArrayList<>();
+        // Intent storeId
+        storeId = 165;
         customView();
-
-        Feedback feedback = new Feedback(1, 1, "Đạt chỉ hơi ngu chút thôi", 1);
-        Feedback feedback1 = new Feedback(1, 1, "Hayyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy yyyyyyyyyyyyyyyyyyyyyyyyyyyyyy yyyyyyyyyyyyyyyyyyyyyyyyyyy", 2);
-        List<Feedback> feedbacks = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            feedbacks.add(feedback);
-            feedbacks.add(feedback1);
-        }
-        Toast.makeText(getBaseContext(), feedbacks.size() + "", Toast.LENGTH_LONG).show();
-
-        FeedbackManagementAdapter feedbackManagementAdapter = new FeedbackManagementAdapter(FeedbackManagement.this, R.layout.item_feedback, feedbacks);
-        ListView listView = findViewById(R.id.content);
-
+        mHandle = new MyHandle();
+        feedbackManagementAdapter = new FeedbackManagementAdapter(FeedbackManagement.this, R.layout.item_feedback, list);
+        listView = findViewById(R.id.content);
         listView.setAdapter(feedbackManagementAdapter);
+        if (storeId != -1){
+            Call<List<UserFeedback>> call = ApiUtils.getAPIService().getAllFeedback(storeId , 0);
+            new ManagementFeedback().execute(call);
+        } else {
+            Toast.makeText(this, "Có lỗi xảy ra", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        LayoutInflater li = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        footerView = li.inflate(R.layout.footer_loading_listview_lazy_loading, null);
+
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                int count = list.size();
+                if (view.getLastVisiblePosition() == totalItemCount - 1 && count == (page * 10) && isLoading == false  && (page > 0)) {
+                    isLoading = true;
+                    Thread thread = new ThreadgetMoreData();
+                    thread.start();
+                }
+            }
+        });
     }
 
     private void customView(){
         CustomInterface.setStatusBarColor(this);
+    }
+
+    public class ManagementFeedback extends AsyncTask<Call,Void,List<UserFeedback>> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(List<UserFeedback> result) {
+            super.onPostExecute(result);
+            if (result == null){
+
+                return;
+            }
+            if (result.size() == 0){
+
+                return;
+            }
+            for (UserFeedback uf : result){
+                list.add(uf);
+            }
+            feedbackManagementAdapter.notifyDataSetChanged();
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected List<UserFeedback> doInBackground(Call... calls) {
+            try {
+                Call<List<UserFeedback>> call = calls[0];
+                Response<List<UserFeedback>> response = call.execute();
+                return response.body();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    public class ThreadgetMoreData extends Thread {
+        @Override
+        public void run() {
+            mHandle.sendEmptyMessage(0);
+            //List<Item> addMoreList = getMoreData();
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (isLoading == true) {
+                Message msg = mHandle.obtainMessage(1);
+                mHandle.sendMessage(msg);
+            }
+        }
+    }
+
+    public void getMoreData(){
+        Log.d("page",String.valueOf(page));
+        callAPI(page);
+        page ++;
+    }
+
+    public class MyHandle extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case 0 :
+                    listView.addFooterView(footerView);
+                    break;
+                case 1:
+                    //adapter.addListItemToAdapter((ArrayList<Item>)msg.obj);
+                    listView.removeFooterView(footerView);
+                    getMoreData();
+                    isLoading = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void callAPI (int page){
+        Call<List<UserFeedback>> call = ApiUtils.getAPIService().getAllFeedback(storeId,page);
+        new ManagementFeedback().execute(call);
     }
 }
--- Capstone Project/Project Deliverables/Draft/Front-end/3sproject/app/src/main/java/project/view/gui/UserSearchProductPage.java
+++ Capstone Project/Project Deliverables/Draft/Front-end/3sproject/app/src/main/java/project/view/gui/UserSearchProductPage.java
@@ -159,11 +159,6 @@ public class UserSearchProductPage extends BasePage {
             }
         });
         loadingBar.getIndeterminateDrawable().setColorFilter(getResources().getColor(R.color.colorApplication), android.graphics.PorterDuff.Mode.MULTIPLY);
-
-        suggestions.add("Sản phẩm kia là abc xyz");
-        suggestions.add("DM");
-        suggestions.add("Cool");
-        suggestions.add("Sản phẩm này là Samsung Galaxy Note 8 128Gb");
         if(getIntent().getStringExtra("code")!=null){
             final Call<List<Product>> listBarcode = mAPI.userSearchBarcode(getIntent().getStringExtra("code"));
             new UserSearchWithBarcode().execute(listBarcode);
@@ -253,7 +248,6 @@ public class UserSearchProductPage extends BasePage {
         });
     }
 
-    //Đây là hàm do Đạt sửa bá đạo
     private void turnOnLocation(){
         final LocationListener locationListener = new LocationListener() {
             public void onLocationChanged(Location location) {
@@ -322,10 +316,6 @@ public class UserSearchProductPage extends BasePage {
         }
     }
 
-
-
-
-
     private void findView(){
 
         noHaveProduct = (TextView) findViewById(R.id.noHaveProduct);
@@ -338,119 +328,6 @@ public class UserSearchProductPage extends BasePage {
         productListView = (ListView) findViewById(R.id.productListView);
         loadingBar = (ProgressBar) findViewById(R.id.loadingBar);
     }
-
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        // thêm search vào vào action bar
-//        getMenuInflater().inflate(R.menu.search_view_with_barcode, menu);
-//        MenuItem itemSearch = menu.findItem(R.id.search_view);
-//        searchView = (SearchView) itemSearch.getActionView();
-////        MenuItem itemSearchWithBarcode = menu.findItem(R.id.search_with_barcode);
-//        searchView.setIconifiedByDefault(false);
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            searchView.setFocusedByDefault(true);
-//        }
-//
-//        searchView.setLayoutParams(new ActionBar.LayoutParams(Gravity.LEFT));
-//
-//        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-//        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-//
-//        final List<String> suggestions = new ArrayList<>();
-//        suggestions.add("Sản phẩm kia là abc xyz");
-//        suggestions.add("DM");
-//        suggestions.add("Cool");
-//        suggestions.add("Sản phẩm này là Samsung Galaxy Note 8 128Gb");
-//
-//
-//        final List<String> searchedList = new ArrayList<>();
-//
-//
-//        final CursorAdapter suggestionAdapter = new SimpleCursorAdapter(this,
-//                R.layout.custom_suggested_listview_nearby_store_page,
-//                null,
-//                new String[]{SearchManager.SUGGEST_COLUMN_TEXT_1},
-//                new int[]{android.R.id.text1},
-//                0);
-//        searchView.setSuggestionsAdapter(suggestionAdapter);
-//
-//
-//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-//            @Override
-//            public boolean onQueryTextSubmit(String query) {
-////                searchedProductList.clear();
-////                page = 1;
-////
-////                query = searchView.getQuery().toString().trim();
-////                SearchProductAddToStore.query = query;
-////                int index = theListView.getFirstVisiblePosition();
-////                View v = theListView.getChildAt(0);
-////                int top = (v == null) ? 0 : v.getTop();
-////                theListView.setSelectionFromTop(index, top);
-////
-////                if(!query.isEmpty()) {
-////                    callAPI(query,page);
-////                }
-//                Toast.makeText(UserSearchProductPage.this, "onQueryTextSubmit : "+ query, Toast.LENGTH_SHORT).show();
-//                int index = productListView.getFirstVisiblePosition();
-//                View v = productListView.getChildAt(0);
-//                int top = (v == null) ? 0 : v.getTop();
-//                productListView.setSelectionFromTop(index, top);
-//                return false;
-//            }
-//
-//
-//            @Override
-//            public boolean onQueryTextChange(String newText) {
-////                Toast.makeText(SearchProductAddToStore.this, "content : "+ newText, Toast.LENGTH_SHORT).show();
-//                String[] columns = { BaseColumns._ID,
-//                        SearchManager.SUGGEST_COLUMN_TEXT_1,
-//                };
-//                final MatrixCursor cursor = new MatrixCursor(columns);
-//                searchedList.removeAll(searchedList);
-//
-//                for (int i = 0; i < suggestions.size(); i++) {
-//                    if (suggestions.get(i).toLowerCase().contains(newText.toLowerCase())) {
-//                        String[] tmp = {Integer.toString(i), suggestions.get(i)};
-//                        cursor.addRow(tmp);
-//
-//                        searchedList.add(suggestions.get(i).toString());
-//                    }
-//                }
-//                suggestionAdapter.swapCursor(cursor);
-//                return true;
-//            }
-//        });
-//
-//        searchView.setOnSuggestionListener(new SearchView.OnSuggestionListener() {
-//            @Override
-//            public boolean onSuggestionSelect(int position) {
-//                searchView.setQuery(searchedList.get(position).toString(), true);
-//                searchView.clearFocus();
-//                return false;
-//            }
-//
-//            @Override
-//            public boolean onSuggestionClick(int position) {
-//                searchView.setQuery(searchedList.get(position).toString(), true);
-//                searchView.clearFocus();
-//                return true;
-//            }
-//        });
-//
-////        itemSearchWithBarcode.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-////            @Override
-////            public boolean onMenuItemClick(MenuItem item) {
-////                Intent intent = new Intent(UserSearchProductPage.this, MainActivity.class);
-////                startActivity(intent);
-////                return false;
-////            }
-////        });
-//
-//        return true;
-//    }
 
     public class UserSearchProductAsyncTask1 extends AsyncTask<Call, Void, List<Product>> {
 
